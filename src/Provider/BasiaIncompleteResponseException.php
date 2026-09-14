054955ab59603cd90bd130bf33012916f0e8ef21
saulgajda <saulgajda@users.noreply.github.com>
2026-09-14 22:36:40 +0200

    Add BASIA incomplete response exception

---
+++ src/Provider/BasiaIncompleteResponseException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyTree\IndexProviders\Provider;
+
+use RuntimeException;
+
+final class BasiaIncompleteResponseException extends RuntimeException
+{
+}
