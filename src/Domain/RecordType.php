d9a038b167c239fc1da95c36da04ebe1dc996248
saulgajda <saulgajda@users.noreply.github.com>
2026-09-14 18:58:28 +0200

    feat: extend canonical index record types

--- src/Domain/RecordType.php
+++ src/Domain/RecordType.php
@@ -9,5 +9,7 @@ enum RecordType: string
     case Birth = 'birth';
     case Marriage = 'marriage';
     case Death = 'death';
+    case Banns = 'banns';
     case ParishCensus = 'parish_census';
+    case Other = 'other';
 }
